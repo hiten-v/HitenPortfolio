dd6cd51b710a1e680c7df571adea376d9c4ff378
Hiten Vaid <hitenvaid3837@gmail.com>
2026-02-05 23:59:13 +0530

    added navbar component struc

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function Navbar() {
+  
+  return (
+    <>
+      <div className="flex justify-center">
+        Hello Hiten here
+        Portfolio 2
+      </div>
+    </>
+  )
+}
+
+export default Navbar
