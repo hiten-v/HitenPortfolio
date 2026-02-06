ca60216bff4b9921e5e27868d45a2572068525df
Hiten Vaid <hitenvaid3837@gmail.com>
2026-02-06 23:57:38 +0530

    added component in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,11 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-
+import Navbar from './components/Navbar'
 function App() {
   
   return (
     <>
+      <Navbar/>
       <div className="flex justify-center">
         Hello Hiten here
         Portfolio 2
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,6 +7,11 @@ function Navbar() {
     <>
       <div className="flex justify-center">
         Navbar
+        <div className="flex flex-col gap-2">
+          <div>Home</div>
+          <div>Contact</div>
+          <div>Help</div>
+        </div>
       </div>
     </>
   )
