95f0b65478e7854984a7299e6c3eebfdc516e85e
Hiten Vaid <hitenvaid3837@gmail.com>
2026-02-07 23:55:09 +0530

    added projects compoment

---
+++ frontend/src/components/Projects.jsx
@@ -0,0 +1,20 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function Projects() {
+  
+  return (
+    <>
+      <div className="flex justify-center">
+        Navbar
+        <div className="flex flex-col gap-2">
+          <div>Home</div>
+          <div>Contact</div>
+          <div>Help</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Projects
