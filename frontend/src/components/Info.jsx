cae7324e6a8f80f7a11de4e1ab825da8d234fa9f
Hiten Vaid <hitenvaid3837@gmail.com>
2026-02-09 23:57:36 +0530

    added info component

---
+++ frontend/src/components/Info.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function Info() {
+  
+  return (
+    <>
+      <div className="flex justify-center">
+        Navbar
+        <div className="flex flex-col gap-2">
+          <div>Link1 </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Info
