6b21d78e16f94d26eb35b86d81c72b9f59fa4119
Hiten Vaid <hitenvaid3837@gmail.com>
2026-02-17 23:56:01 +0530

    added new comp

---
+++ frontend/src/components/Np.jsx
@@ -0,0 +1,18 @@
+import { useState, useEffect } from 'react'
+import './App.css'
+
+function Np() {
+  
+  return (
+    <>
+      <div className="flex justify-center">
+        Navbar
+        <div className="flex flex-col gap-2">
+          <div>Link1 </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Np
